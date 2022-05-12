d96fd2ca901d0019f748d6ffd95d25eead6b00d4
BloodTaint <bloodtaint@yahoo.com>
2022-05-12 15:54:22 +0300

    12.05.22/3

--- Polymorphism and Abstraction - Exercise/account.py
+++ Polymorphism and Abstraction - Exercise/account.py
@@ -1,3 +1,40 @@
+class Account:
+    def __init__(self, owner, amount=0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+
+        self._transactions.append(amount)
+
+    @staticmethod
+    def validate_transaction(account, amount_to_add):
+        new_balance = account.balance + amount_to_add
+        if new_balance < 0:
+            raise ValueError("sorry cannot go in debt!")
+        account.add_transaction(amount_to_add)
+        return f"New balance: {account.balance}"
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, idx):
+        return self._transactions[idx]
+
+
 acc = Account('bob', 10)
 acc2 = Account('john')
 print(acc)
