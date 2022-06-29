4bff9c8f4a5c0c15fd62398f4118fc7c2fe18946
bloodtaint <bloodtaint@yahoo.com>
2022-06-29 15:11:24 +0300

    29.06.22

--- 02.first_steps_in_oop_exercise/pokemon/project/pokemon.py
+++ 02.first_steps_in_oop_exercise/pokemon/project/pokemon.py
@@ -1,8 +1,16 @@
 class Pokemon:
-    def __init__(self,name,health):
+    def __init__(self, name, health):
         self.name = name
         self.health = health
 
     def pokemon_details(self):
         return f"{self.name} with health {self.health}"
- 
+
+# class Pokemon:
+#     def __init__(self,name,health):
+#         self.name = name
+#         self.health = health
+#
+#     def pokemon_details(self):
+#         return f"{self.name} with health {self.health}"
+#
--- 02.first_steps_in_oop_exercise/pokemon/project/trainer.py
+++ 02.first_steps_in_oop_exercise/pokemon/project/trainer.py
@@ -2,29 +2,63 @@ from project.pokemon import Pokemon
 
 
 class Trainer:
-    def __init__(self, name: str):
+    def __init__(self, name):
         self.name = name
         self.pokemons = []
 
     def add_pokemon(self, pokemon: Pokemon):
-        if any([x for x in self.pokemons if x.name == pokemon.name]):
-            return "This pokemon is already caught"
+        for this_pokemon in self.pokemons:
+            if this_pokemon.name == pokemon.name:
+                return "This pokemon is already caught"
 
         self.pokemons.append(pokemon)
+        # return f"Caught {pokemon.name} with health {pokemon.health}"
         return f"Caught {pokemon.pokemon_details()}"
 
-    def release_pokemon(self, pokemon_name: str):
-        for pokemon in self.pokemons:
-            if pokemon.name == pokemon_name:
-                self.pokemons.remove(pokemon)
+    def release_pokemon(self, pokemon_name):
+        for this_pokemon in self.pokemons:
+            if this_pokemon.name == pokemon_name:
+                self.pokemons.remove(this_pokemon)
                 return f"You have released {pokemon_name}"
-
         return "Pokemon is not caught"
 
     def trainer_data(self):
-        data = f"Pokemon Trainer {self.name}\n"
-        data += f"Pokemon count {len(self.pokemons)}\n"
-        for pokemon in self.pokemons:
-            data += f"- {pokemon.pokemon_details()}\n"
-        return data
 
+        information = ''
+        information += f'Pokemon Trainer {self.name}' + '\n'
+        information += f'Pokemon count {len(self.pokemons)}' + '\n'
+        for i in self.pokemons:
+            information += f'- {i.pokemon_details()}' + '\n'
+        return information.strip()
+
+
+# from project.pokemon import Pokemon
+#
+#
+# class Trainer:
+#     def __init__(self, name: str):
+#         self.name = name
+#         self.pokemons = []
+#
+#     def add_pokemon(self, pokemon: Pokemon):
+#         if any([x for x in self.pokemons if x.name == pokemon.name]):
+#             return "This pokemon is already caught"
+#
+#         self.pokemons.append(pokemon)
+#         return f"Caught {pokemon.pokemon_details()}"
+#
+#     def release_pokemon(self, pokemon_name: str):
+#         for pokemon in self.pokemons:
+#             if pokemon.name == pokemon_name:
+#                 self.pokemons.remove(pokemon)
+#                 return f"You have released {pokemon_name}"
+#
+#         return "Pokemon is not caught"
+#
+#     def trainer_data(self):
+#         data = f"Pokemon Trainer {self.name}\n"
+#         data += f"Pokemon count {len(self.pokemons)}\n"
+#         for pokemon in self.pokemons:
+#             data += f"- {pokemon.pokemon_details()}\n"
+#         return data
+#
