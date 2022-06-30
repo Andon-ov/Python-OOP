fa1c784a9a9ac9d54a51066a35c7c8d1d0d136d7
bloodtaint <bloodtaint@yahoo.com>
2022-06-30 10:22:20 +0300

    29.06.22

--- 04.classes_and_objects_exercise/Account.py
+++ 04.classes_and_objects_exercise/Account.py
@@ -1,3 +1,41 @@
+"""
+The class should also have 3 additional instance methods:
+-credit(amount) - adds the amount to the balance and returns the new balance
+
+-debit(amount) - if the amount is less than or equal to the balance,
+reduces the balance by the amount and returns the new balance.
+
+Otherwise, return "Amount exceeded balance"
+-info() - returns "User {name} with account {id} has {balance} balance"
+"""
+
+
+class Account:
+    def __init__(self, id, name, balance=0):
+        self.id = id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount):
+        pass
+
+    def debit(self, amount):
+        pass
+
+    def info(self):
+        pass
+
+
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
+print(account.info())
+account = Account(5411256, "Peter")
+print(account.debit(500))
+print(account.credit(1000))
+print(account.debit(500))
+print(account.info())
+
 # class Account:
 #     def __init__(self, id, name, balance=0):
 #         self.id = id
@@ -19,44 +57,21 @@
 #         return f"User {self.name} with account {self.id} has {self.balance} balance"
 
 
-"""
-The class should also have 3 additional instance methods:
--credit(amount) - adds the amount to the balance and returns the new balance
-
--debit(amount) - if the amount is less than or equal to the balance,
-reduces the balance by the amount and returns the new balance.
-
-Otherwise, return "Amount exceeded balance"
--info() - returns "User {name} with account {id} has {balance} balance"
-"""
-
-
-class Account:
-    def __init__(self, id, name, balance=0):
-        self.id = id
-        self.name = name
-        self.balance = balance
-
-    def credit(self, amount):
-        self.balance += amount
-        return self.balance
-
-    def debit(self, amount):
-        if not amount <= self.balance:
-            return "Amount exceeded balance"
-        self.balance -= amount
-        return self.balance
-
-    def info(self):
-        return f"User {self.name} with account {self.id} has {self.balance} balance"
-
-
-account = Account(1234, "George", 1000)
-print(account.credit(500))
-print(account.debit(1500))
-print(account.info())
-account = Account(5411256, "Peter")
-print(account.debit(500))
-print(account.credit(1000))
-print(account.debit(500))
-print(account.info())
+# class Account:
+#     def __init__(self, id, name, balance=0):
+#         self.id = id
+#         self.name = name
+#         self.balance = balance
+# 
+#     def credit(self, amount):
+#         self.balance += amount
+#         return self.balance
+# 
+#     def debit(self, amount):
+#         if not amount <= self.balance:
+#             return "Amount exceeded balance"
+#         self.balance -= amount
+#         return self.balance
+# 
+#     def info(self):
+#         return f"User {self.name} with account {self.id} has {self.balance} balance"
--- 04.classes_and_objects_exercise/Time.py
+++ 04.classes_and_objects_exercise/Time.py
@@ -1,34 +1,3 @@
-# class Time:
-#     max_hours = 23
-#     max_minutes = 59
-#     max_seconds = 59
-#
-#     def __init__(self, hours, minutes, seconds):
-#         self.hours = hours
-#         self.minutes = minutes
-#         self.seconds = seconds
-#
-#     def set_time(self, new_hours, new_minutes, new_seconds):
-#         self.hours = new_hours
-#         self.minutes = new_minutes
-#         self.seconds = new_seconds
-#
-#     def get_time(self):
-#         return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
-#
-#     def next_second(self):
-#         self.seconds += 1
-#         if self.seconds > Time.max_seconds:
-#             self.seconds = 0
-#             self.minutes += 1
-#             if self.minutes > Time.max_minutes:
-#                 self.minutes = 0
-#                 self.hours += 1
-#                 if self.hours > Time.max_hours:
-#                     self.hours = 0
-#         return self.get_time()
-
-
 """ Create a class called Time. Upon initialization, it should receive hours, minutes, and seconds (integers).
 The class should also have class attributes max_hours equal to 23, max_minutes equal to 59, and max_seconds equal to 59.
 You should also create 3 additional instance methods:
@@ -59,18 +28,18 @@ class Time:
     def next_second(self):
         self.seconds += 1
 
-        if self.seconds >= Time.max_seconds:
+        if self.seconds >= self.max_seconds:
             self.seconds = 0
             self.minutes += 1
 
-        if self.minutes >= Time.max_minutes:
+        if self.minutes >= self.max_minutes:
             self.minutes = 0
             self.hours += 1
 
-        if self.hours >= Time.max_hours:
+        if self.hours >= self.max_hours:
             self.hours = 0
 
-        return Time.get_time(self)
+        return self.get_time()
 
 
 time = Time(9, 30, 59)
@@ -79,3 +48,67 @@ time = Time(10, 59, 59)
 print(time.next_second())
 time = Time(23, 59, 59)
 print(time.next_second())
+
+# class Time:
+#     max_hours = 23
+#     max_minutes = 59
+#     max_seconds = 59
+#
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def set_time(self, new_hours, new_minutes, new_seconds):
+#         self.hours = new_hours
+#         self.minutes = new_minutes
+#         self.seconds = new_seconds
+#
+#     def get_time(self):
+#         return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+#
+#     def next_second(self):
+#         self.seconds += 1
+#
+#         if self.seconds >= Time.max_seconds:
+#             self.seconds = 0
+#             self.minutes += 1
+#
+#         if self.minutes >= Time.max_minutes:
+#             self.minutes = 0
+#             self.hours += 1
+#
+#         if self.hours >= Time.max_hours:
+#             self.hours = 0
+#
+#         return Time.get_time(self)
+
+# class Time:
+#     max_hours = 23
+#     max_minutes = 59
+#     max_seconds = 59
+#
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def set_time(self, new_hours, new_minutes, new_seconds):
+#         self.hours = new_hours
+#         self.minutes = new_minutes
+#         self.seconds = new_seconds
+#
+#     def get_time(self):
+#         return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+#
+#     def next_second(self):
+#         self.seconds += 1
+#         if self.seconds > Time.max_seconds:
+#             self.seconds = 0
+#             self.minutes += 1
+#             if self.minutes > Time.max_minutes:
+#                 self.minutes = 0
+#                 self.hours += 1
+#                 if self.hours > Time.max_hours:
+#                     self.hours = 0
+#         return self.get_time()
