b14e16aaa9cb76ecd56bf9218004649ff36d6bb3
BloodTaint <Bloodtaint@yahoo.com>
2022-07-04 16:47:04 +0300

    04.07.22

--- 05.inheritance/Stack of Strings/project/stack.py
+++ 05.inheritance/Stack of Strings/project/stack.py
@@ -1,49 +1,22 @@
+class Stack:
+    def __init__(self):
+        self.data: list[str] = []
 
+    def push(self, element):
+        self.data.append(element)
 
+    def pop(self):
+        return self.data.pop()
 
+    def top(self):
+        return self.data[-1]
 
+    def is_empty(self):
+        return False if len(self.data) > 0 else True
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def __str__(self):
+        result = self.data[::-1]
+        return f"[{', '.join([str(x) for x in self.data[::-1]])}]"
 
 # from typing import List
 
@@ -66,6 +39,8 @@
 #
 #     def __str__(self):
 #         return "[" + ", ".join(reversed(self.data)) + "]"
+
+
 # class Stack:
 #     def __init__(self):
 #         self.data = []
@@ -86,5 +61,3 @@
 #
 #     def __str__(self):
 #         return f"[{', '.join([x for x in self.data])}]"
-
-
