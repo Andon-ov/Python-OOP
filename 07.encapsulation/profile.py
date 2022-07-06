7f161bc973894a89896baad3348090d180354cba
bloodtaint <bloodtaint@yahoo.com>
2022-07-06 07:33:08 +0300

    29.06.22

--- 07.encapsulation/profile.py
+++ 07.encapsulation/profile.py
@@ -1,4 +1,11 @@
 class Profile:
+    min_username_len = 5
+    max_username_len = 15
+
+    min_password_len = 8
+    min_digit_len = 1
+    min_uppercase_letter_len = 1
+
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
@@ -9,7 +16,7 @@ class Profile:
 
     @username.setter
     def username(self, value):
-        if len(value) < 5 or len(value) > 15:
+        if len(value) < self.min_username_len or len(value) > self.max_username_len:
             raise ValueError("The username must be between 5 and 15 characters.")
         self.__username = value
 
@@ -19,24 +26,23 @@ class Profile:
 
     @password.setter
     def password(self, value):
-        if not any([x.isdigit() for x in value]) and not any([x.isupper() for x in value]) and not len(self.password) >= 8:
+        is_length_valid = len(value) >= self.min_password_len
+        is_upper_case_presented = any([True for x in value if x.isupper()])
+        is_digit_presented = any([True for x in value if x.isdigit()])
+
+        if is_length_valid and is_upper_case_presented and is_digit_presented:
+            self.__password = value
+        else:
             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
-        self.__password = value
-
-    # the password must be at least 8 characters long; it must contain at least one upper case letter and at least one digit.
-
 
     def __str__(self):
         return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
 
 
-profile_with_invalid_password = Profile('My_username', 'My-password')
-profile_with_invalid_username = Profile('Too_long_username', 'Any')
+# profile_with_invalid_password = Profile('My_username', 'My-password')
+# profile_with_invalid_username = Profile('Too_long_username', 'Any')
 correct_profile = Profile("Username", "Passw0rd")
 print(correct_profile)
-
-
-
 
 # class Profile:
 #     min_username_len = 5
