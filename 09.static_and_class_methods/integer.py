b94370b27fb5637ba7536cfb657bcd50fda04606
BloodTaint <Bloodtaint@yahoo.com>
2022-07-08 19:52:56 +0300

    08.07.22

--- 09.static_and_class_methods/integer.py
+++ 09.static_and_class_methods/integer.py
@@ -1,36 +1,44 @@
+from math import floor
 
 
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if isinstance(float_value, float):
+            return cls(floor(float_value))
+        return "value is not a float"
+
+    @classmethod
+    def from_roman(cls, value):
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        summ= 0
+        for i in range(len(value)-1,-1,-1):
+            num = roman[value[i]]
+            if 3*num < summ:
+                summ = summ-num
+            else:
+                summ = summ+num
+        value = summ
+        return cls(value)
+
+    @classmethod
+    def from_string(cls, value):
+        if isinstance(value, str):
+            return cls(int(value))
+        return "wrong type"
 
 
+first_num = Integer(10)
+print(first_num.value)
 
+second_num = Integer.from_roman("IV")
+print(second_num.value)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
 
 # class Integer:
 #     def __init__(self, value):
