bcf542f199b9f555d906f4cc9655986482002f00
BloodTaint <Bloodtaint@yahoo.com>
2022-07-08 18:18:58 +0300

    08.07.22

--- 09.static_and_class_methods/shop.py
+++ 09.static_and_class_methods/shop.py
@@ -1,10 +1,24 @@
+class Shop:
+    pass
 
+# Create a class called Shop. Upon initialization, it should receive a name (str), type (str), capacity (int). The store should also have an attribute called items (an empty dictionary that stores the name of an item and its quantity). The class should have 4 methods:
+# small_shop(name: str, type: str) - a new shop with a capacity of 10 should be created
+# add_item(item_name:str) - adds 1 to the quantity of the given item. On success, the method should return "{item_name} added to the shop". If the addition is not possible, the following message should be returned "Not enough capacity in the shop"
+# remove_item(item_name:str, amount:int) - removes the given amount from the item. On success, it should return "{amount} {item_name} removed from the shop". Otherwise, the method should return "Cannot remove {amount} {item_name}"
+# oIf the item quantity reaches 0, the item should be removed from the items' dictionary.
+# __repr__() - returns a string representation in the format "{shop_name} of type {shop_type} with capacity {shop_capacity}"
+fresh_shop = Shop("Fresh Shop", "Fruit and Veg", 50)
+small_shop = Shop.small_shop("Fashion Boutique", "Clothes")
+print(fresh_shop)
+print(small_shop)
 
+print(fresh_shop.add_item("Bananas"))
+print(fresh_shop.remove_item("Tomatoes", 2))
 
-
-
-
-
+print(small_shop.add_item("Jeans"))
+print(small_shop.add_item("Jeans"))
+print(small_shop.remove_item("Jeans", 2))
+print(small_shop.items)
 
 
 
