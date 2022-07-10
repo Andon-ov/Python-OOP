62ef0f6cb8fd149b7965631a3d114535f467a84a
BloodTaint <Bloodtaint@yahoo.com>
2022-07-10 19:53:39 +0300

    10.07.22

--- 10.static_and_class_methods_exercise/Movie World/project/movie_world.py
+++ 10.static_and_class_methods_exercise/Movie World/project/movie_world.py
@@ -1,69 +1,65 @@
+from project.customer import Customer
+from project.dvd import DVD
 
 
+class MovieWorld:
+    def __init__(self, name: str):
+        self.name = name
+        self.customers = []
+        self.dvds = []
 
+    @staticmethod
+    def dvd_capacity():
+        result = 15
+        return result
 
+    @staticmethod
+    def customer_capacity():
+        result = 10
+        return result
 
+    def add_customer(self, customer: Customer):
 
+        if len(self.customers) < MovieWorld.customer_capacity():
+            self.customers.append(customer)
 
+    def add_dvd(self, dvd: DVD):
+        if len(self.dvds) < MovieWorld.dvd_capacity():
+            self.dvds.append(dvd)
 
+    def rent_dvd(self, customer_id: int, dvd_id: int):
+        customer = []
+        dvd = []
 
+        for i in self.customers:
+            if i.id == customer_id:
+                customer.append(i)
+                for j in i.rented_dvds:
+                    if j.id == dvd_id:
+                        dvd.append(j)
+                        return f"{i.name} has already rented {j.name}"
 
+        if dvd[0].age_restriction > customer[0].age:
+            return f"{customer[0].name} should be at least {dvd[0].age_restriction} to rent this movie"
 
+        if dvd[0].is_rented:
+            return "DVD is already rented"
+        customer[0].rented_dvds.append(dvd[0])
+        return f"{customer[0].name} has successfully rented {dvd[0].name}"
 
+    def return_dvd(self, customer_id, dvd_id):
+        for i in self.customers:
+            if i.id == customer_id:
+                for j in i.rented_dvds:
+                    if j.id == dvd_id:
+                        return f"{i.name} has successfully returned {j.name}"
+        return "{customer_name} does not have that DVD"
 
+    def __repr__(self):
+        result = ''
+        for i in self.customers:
+            result += i.Customer() + '\n'
+        for j in self.dvds:
+            result += j.Dvd() + '\n'
+        return result
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from project.customer import Customer
-# from project.dvd import DVD
-#
-#
-# class MovieWorld:
-#     def __init__(self, name):
-#         self.name = name
-#         self.customers = []
-#         self.dvds = []
-#
-#     def add_customer(self, customer: Customer):
-#         pass
-#
-#     def add_dvd(self, dvd: DVD):
-#         pass
-#
-#     def rent_dvd(self, customer_id: int, dvd_id: int):
-#         pass
-#
-#     def dvd_capacity():
-#         return 15  # the dvd capacity of a movie world
-#
-#     def customer_capacity()
-#         return 10
-#
-#     def return_dvd(customer_id, dvd_id):
-#         pass
-#     def __repr__():
-#
--- 10.static_and_class_methods_exercise/photo_album.py
+++ 10.static_and_class_methods_exercise/photo_album.py
@@ -1,32 +1,108 @@
+from math import ceil
+
+
 class PhotoAlbum:
+    PHOTOS_PER_PAGE = 4
+
     def __init__(self, pages):
         self.pages = pages
-        self.photos = []
+        self.photos = self.__init_photos(pages)
 
-    # Each page can contain only 4 photos.
+    @staticmethod
+    def __init_photos(pages):
+        result = []
+        for _ in range(pages):
+            result.append([])
+        return result
+
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        pass
+        pages = ceil(photos_count // PhotoAlbum.PHOTOS_PER_PAGE)
+        return cls(pages)
 
     def add_photo(self, label: str):
-        pass
-        # – adds the photo in the first possible page and slot and return
-        # "{label} photo added successfully on page {page_number(starting from 1)} slot {slot_number(starting from 1)}".
-        # If there are no free slots left, return "No more free slots"
+        for idx, page in enumerate(self.photos):
+            if len(page) < PhotoAlbum.PHOTOS_PER_PAGE:
+                self.photos[idx].append(label)
+                return f"{label} photo added successfully on page {idx + 1} slot {len(self.photos[idx])}"
+                # page.append(label)
+                # return f"{label} photo added successfully on page {idx + 1} slot {len(page)}"
+
+        return "No more free slots"
 
     def display(self):
-        pass
-        # For example, if we have 1 page and 2 photos:
-        # -----------
-        # [] []
-        # -----------
-        # and if we have 2 empty pages:
-        # -----------
-        #
-        # -----------
-        #
-        # -----------
+        sep = "-" * 11
+        result = sep + '\n'
+        for row in self.photos:
+            result += ' '.join(['[]' for _ in row]) + '\n'
+            result += sep + '\n'
 
+        return result.strip()
+
+#ToDo 85/100
+
+# album = PhotoAlbum(2)
+
+# print(album.add_photo("baby"))
+# print(album.add_photo("first grade"))
+# print(album.add_photo("eight grade"))
+# print(album.add_photo("party with friends"))
+# print(album.photos)
+# print(album.add_photo("prom"))
+# print(album.add_photo("wedding"))
+#
+# print(album.display())
+
+import unittest
+
+
+class TestsPhotoAlbum(unittest.TestCase):
+    def test_init_creates_all_attributes(self):
+        album = PhotoAlbum(2)
+        self.assertEqual(album.pages, 2)
+        self.assertEqual(album.photos, [[], []])
+
+    def test_from_photos_should_create_instace(self):
+        album = PhotoAlbum.from_photos_count(12)
+        self.assertEqual(album.pages, 3)
+        self.assertEqual(album.photos, [[], [], []])
+
+    def test_add_photo_with_no_free_spots(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        result = album.add_photo("prom")
+        self.assertEqual(result, "No more free slots")
+
+    def test_add_photo_with_free_spots(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        self.assertEqual(album.photos, [['baby', 'first grade', 'eight grade', 'party with friends']])
+
+    def test_display_with_one_page(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        result = album.display().strip()
+        self.assertEqual(result, "-----------\n[] [] [] []\n-----------")
+
+    def test_display_with_three_pages(self):
+        album = PhotoAlbum(3)
+        for _ in range(8):
+            album.add_photo("asdf")
+        result = album.display().strip()
+        self.assertEqual(result, "-----------\n[] [] [] []\n-----------\n[] [] [] []\n-----------\n\n-----------")
+
+
+if __name__ == "__main__":
+    unittest.main()
 # from math import ceil
 #
 #
@@ -65,14 +141,3 @@ class PhotoAlbum:
 #         return result
 #
 #
-# album = PhotoAlbum(2)
-#
-# print(album.add_photo("baby"))
-# print(album.add_photo("first grade"))
-# print(album.add_photo("eight grade"))
-# print(album.add_photo("party with friends"))
-# print(album.photos)
-# print(album.add_photo("prom"))
-# print(album.add_photo("wedding"))
-#
-# print(album.display())
