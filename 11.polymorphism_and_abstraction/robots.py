f88bf5ffbf1d15951a13adaf8366f37872574214
BloodTaint <Bloodtaint@yahoo.com>
2022-07-11 20:59:55 +0300

    11.07.22

--- 11.polymorphism_and_abstraction/robots.py
+++ 11.polymorphism_and_abstraction/robots.py
@@ -1,25 +1,39 @@
+class Robot:
+    sensors = 1
+
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def sensors_amount(cls):
+        return cls.sensors
 
 
+class MedicalRobot(Robot):
+    sensors = 6
 
 
+class ChefRobot(Robot):
+    sensors = 4
 
 
+class WarRobot(Robot):
+    sensors = 12
 
 
+def number_of_robot_sensors(robot):
+    print(robot.sensors_amount())
 
 
+basic_robot = Robot('Robo')
+da_vinci = MedicalRobot('Da Vinci')
+moley = ChefRobot('Moley')
+griffin = WarRobot('Griffin')
 
-
-
-
-
-
-
-
-
-
-
-
+number_of_robot_sensors(basic_robot)
+number_of_robot_sensors(da_vinci)
+number_of_robot_sensors(moley)
+number_of_robot_sensors(griffin)
 
 # class Robot:
 #     def __init__(self, name):
