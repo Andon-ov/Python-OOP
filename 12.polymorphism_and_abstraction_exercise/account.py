758d41dc80daa2559a4a7acd83fc81c4a13c1fc4
BloodTaint <Bloodtaint@yahoo.com>
2022-07-12 16:34:07 +0300

    11.07.22

--- 12.polymorphism_and_abstraction_exercise/account.py
+++ 12.polymorphism_and_abstraction_exercise/account.py
@@ -1,6 +1,66 @@
 
 
+class Account:
+    def __init__(self,owner,amount=0 ):
+        self.owner = owner
+        self.amount = amount
+        self._transactions =[]
 
+
+
+    def add_transaction(self, amount):
+        pass
+
+    # if the amount is not an integer, raise ValueError with the message
+    # "please use int for amount". Otherwise, add the amount to the transactions
+
+
+    def balance(self):
+        return
+        #  the sum between the amount and all the transactions
+
+    def validate_transaction(self,account: Account, amount_to_add):
+
+        # ◦ If the balance becomes less than zero, raise ValueError with the message "sorry cannot go in debt!" and break the transaction.
+        # ◦ Otherwise, complete it and return a message "New balance: {account_balance}"
+
+# Implement the correct magic methods so the code in the example below works properly:
+#     • When you print an account instance, the output should be in the format "Account of {owner} with starting amount: {amount}".
+#     • When you print a representational string of an account instance, the output should be in the format "Account({owner}, {amount})".
+#     • When you access the length of an account instance, you should receive the total number of transactions made.
+#     • You should iterate over an account instance and receive each transaction as a result.
+#     • You should be able to reverse the order of transactions by reversing an account instance.
+#     • You should be able to compare (>, <, >=, <=, ==, !=) two account instances by their balance amount.
+#     • When you concatenate two accounts, you should return a new account with a name - string in the format "{first_owner}&{second_owner}" and starting amount - the sum between their two. Both their transactions should be added to the new account.
+
+
+
+
+
+acc = Account('bob', 10)
+acc2 = Account('john')
+print(acc)
+print(repr(acc))
+acc.add_transaction(20)
+acc.add_transaction(-20)
+acc.add_transaction(30)
+print(acc.balance)
+print(len(acc))
+for transaction in acc:
+    print(transaction)
+print(acc[1])
+print(list(reversed(acc)))
+acc2.add_transaction(10)
+acc2.add_transaction(60)
+print(acc > acc2)
+print(acc >= acc2)
+print(acc < acc2)
+print(acc <= acc2)
+print(acc == acc2)
+print(acc != acc2)
+acc3 = acc + acc2
+print(acc3)
+print(acc3._transactions)
 
 
 
--- 12.polymorphism_and_abstraction_exercise/vehicles.py
+++ 12.polymorphism_and_abstraction_exercise/vehicles.py
@@ -1,35 +1,62 @@
+from abc import ABC, abstractmethod
 
 
+class Vehicle(ABC):
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
 
 
+class Car(Vehicle):
+    fuel_consumption_per_km_with_air_conditioners = 0.9
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        air_conditioners = distance * self.fuel_consumption_per_km_with_air_conditioners
+        consumption = (distance * self.fuel_consumption) + air_conditioners
+        if consumption <= self.fuel_quantity:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
 
 
+class Truck(Vehicle):
+    fuel_consumption_per_km_with_air_conditioners = 1.6
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        air_conditioners = distance * self.fuel_consumption_per_km_with_air_conditioners
+        consumption = (distance * self.fuel_consumption) + air_conditioners
+        if consumption <= self.fuel_quantity:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += (fuel * 0.95)
+        # keeps only 95% of the given fuel
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
 
 # from abc import ABC, abstractmethod
 #
