812b96cf84a4a6e61e9fbfcd5b47201e69f5aeab
BloodTaint <Bloodtaint@yahoo.com>
2022-07-12 17:52:52 +0300

    12.07.22

--- 12.polymorphism_and_abstraction_exercise/account.py
+++ 12.polymorphism_and_abstraction_exercise/account.py
@@ -1,39 +1,57 @@
-
-
 class Account:
-    def __init__(self,owner,amount=0 ):
+    def __init__(self, owner, amount=0):
         self.owner = owner
         self.amount = amount
-        self._transactions =[]
-
-
+        self._transactions = []
 
     def add_transaction(self, amount):
-        pass
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        self._transactions.append(amount)
 
-    # if the amount is not an integer, raise ValueError with the message
-    # "please use int for amount". Otherwise, add the amount to the transactions
-
-
+    @property
     def balance(self):
-        return
-        #  the sum between the amount and all the transactions
+        return self.amount + sum(self._transactions)
 
-    def validate_transaction(self,account: Account, amount_to_add):
+    @staticmethod
+    def validate_transaction(account, amount_to_add):
+        new_balance = account.balance + amount_to_add
+        if new_balance < 0:
+            raise ValueError("sorry cannot go in debt!")
+        account.add_transaction(amount_to_add)
+        return f"New balance: {account.balance}"
 
-        # ◦ If the balance becomes less than zero, raise ValueError with the message "sorry cannot go in debt!" and break the transaction.
-        # ◦ Otherwise, complete it and return a message "New balance: {account_balance}"
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
 
-# Implement the correct magic methods so the code in the example below works properly:
-#     • When you print an account instance, the output should be in the format "Account of {owner} with starting amount: {amount}".
-#     • When you print a representational string of an account instance, the output should be in the format "Account({owner}, {amount})".
-#     • When you access the length of an account instance, you should receive the total number of transactions made.
-#     • You should iterate over an account instance and receive each transaction as a result.
-#     • You should be able to reverse the order of transactions by reversing an account instance.
-#     • You should be able to compare (>, <, >=, <=, ==, !=) two account instances by their balance amount.
-#     • When you concatenate two accounts, you should return a new account with a name - string in the format "{first_owner}&{second_owner}" and starting amount - the sum between their two. Both their transactions should be added to the new account.
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
 
+    def __len__(self):
+        return len(self._transactions)
 
+    def __reversed__(self):
+        return reversed(self._transactions)
+
+    def __getitem__(self, item):
+        return self._transactions[item]
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __le__(self, other):
+        return self.balance <= other.balance
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __add__(self, other):
+        result = Account(f"{self.owner}&{other.owner}", (self.amount + other.amount))
+        for i in self:
+            result.add_transaction(i)
+        for j in other:
+            result.add_transaction(j)
+        return result
 
 
 
@@ -61,26 +79,6 @@ print(acc != acc2)
 acc3 = acc + acc2
 print(acc3)
 print(acc3._transactions)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # class Account:
 #     def __init__(self, owner, amount=0):
