1eebb6319c2cb3beda9bb605ac4b4481601f2be5
BloodTaint <Bloodtaint@yahoo.com>
2022-07-13 11:16:16 +0300

    13.07.22

--- 12.polymorphism_and_abstraction_exercise/Wild_Farm/project/animals/birds.py
+++ 12.polymorphism_and_abstraction_exercise/Wild_Farm/project/animals/birds.py
@@ -1,31 +1,28 @@
+from project.animals.animal import Bird
 
 
+class Owl(Bird):
+    ALLOWED_FOODS = ['Meat']
+    WEIGHT_INCREMENTAL = 0.25
+
+    def __init__(self, name: str, weight: float, wing_size):
+        super().__init__(name, weight, wing_size)
+        self.food_eaten = 0
+
+    def make_sound(self):
+        return "Hoot Hoot"
 
 
+class Hen(Bird):
+    ALLOWED_FOODS = ['Meat', 'Fruit', 'Seed', 'Vegetable']
+    WEIGHT_INCREMENTAL = 0.35
 
+    def __init__(self, name: str, weight: float, wing_size):
+        super().__init__(name, weight, wing_size)
+        self.food_eaten = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def make_sound(self):
+        return "Cluck"
 
 
 
--- 12.polymorphism_and_abstraction_exercise/Wild_Farm/project/animals/mammals.py
+++ 12.polymorphism_and_abstraction_exercise/Wild_Farm/project/animals/mammals.py
@@ -1,31 +1,54 @@
+from project.animals.animal import Mammal
+
+
+class Mouse(Mammal):
+    ALLOWED_FOODS = ['Fruit', 'Vegetable']
+    WEIGHT_INCREMENTAL = 0.10
+
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+        self.food_eaten = 0
+
+    def make_sound(self):
+        return "Squeak"
 
 
 
 
+class Dog(Mammal):
+    ALLOWED_FOODS = ['Meat']
+    WEIGHT_INCREMENTAL = 0.40
+
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+        self.food_eaten = 0
+
+    def make_sound(self):
+        return "Woof!"
 
 
+class Cat(Mammal):
+    ALLOWED_FOODS = ['Meat', 'Vegetable']
+    WEIGHT_INCREMENTAL = 0.30
+
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+        self.food_eaten = 0
+
+    def make_sound(self):
+        return "Meow"
 
 
+class Tiger(Mammal):
+    ALLOWED_FOODS = ['Meat']
+    WEIGHT_INCREMENTAL = 1
 
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+        self.food_eaten = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def make_sound(self):
+        return "ROAR!!!"
 
 # from project.animals.animal import Mammal
 #
