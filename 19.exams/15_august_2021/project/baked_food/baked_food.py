be0010c78c4dfd384e0aa960f9913a1cdff9f43e
BloodTaint <Bloodtaint@yahoo.com>
2022-07-16 18:13:19 +0300

    16.07.22

--- 19.exams/15_august_2021/project/baked_food/baked_food.py
+++ 19.exams/15_august_2021/project/baked_food/baked_food.py
@@ -1,33 +1,31 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
+
+from project.validator import Validator
 
 
 class BakedFood(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, portion: float, price: float):
+        self.name = name
+        self.portion = portion  # It represents the size of the baked food in grams.
+        self.price = price
 
+    @property
+    def name(self):
+        return self.__name
 
+    @name.setter
+    def name(self, value):
+        Validator.raise_error_if_empty_string_or_whitespace(value, "Name cannot be empty string or white space!")
+        self.__name = value
 
-#
-# name: string - passed upon initialization.
-#
-# If the name is an empty string or whitespace, raise a ValueError with the message: "Name cannot be empty string or white space!"
-#
-# portion: float - passed upon initialization. It represents the size of the baked food in grams.
-#
-# price: float - passed upon initialization.
-#
-# If the price is less than or equal to 0, raise a ValueError with the message: "Price cannot be less than or equal to zero!"
-#
-# Methods
-#
-# __init__(name: str, portion: float, price: float)
-#
-# The __init__ method should have a name, portion, and price.
-#
-# __repr__()
-#
-# Override the repr method, so it returns a string with information about each food in the following format:
-#
-# " - {baked_food_name}: {portion}g - {price}lv"
-#
-# The portion size and the price should be formatted to the second decimal point.
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        Validator.raise_error_if_price_less_than_or_equal_to_zero(value, "Price cannot be less than or equal to zero!")
+        self.__price = value
+
+    def __repr__(self):
+        return f" - {self.name}: {self.portion:.02f}g - {self.price:.02f}lv"
--- 19.exams/15_august_2021/project/validator.py
+++ 19.exams/15_august_2021/project/validator.py
@@ -1,4 +1,12 @@
 class Validator:
 
     @staticmethod
-    def raise_error_if_empty_string_or_whitespace(string:str,messege:str):
+    def raise_error_if_empty_string_or_whitespace(string:str,message:str):
+        if string == '' or string == ' ':
+            raise ValueError(message)
+
+
+    @staticmethod
+    def raise_error_if_price_less_than_or_equal_to_zero(value,message:str):
+        if value <= 0:
+            raise ValueError(message)
