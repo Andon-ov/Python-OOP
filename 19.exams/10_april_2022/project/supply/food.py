6acb60dd78b1fa1fcebce50ef602a7e212ecc265
BloodTaint <Bloodtaint@yahoo.com>
2022-07-20 13:24:50 +0300

    20.07.22

--- 19.exams/10_april_2022/project/supply/food.py
+++ 19.exams/10_april_2022/project/supply/food.py
@@ -5,8 +5,6 @@ class Food(Supply):
     def __init__(self, name: str, energy: int = 25):
         super().__init__(name, energy)
 
-        # food has 25 units of energy as an optional parameter.
 
-    #
     # def details(self):
     #     return f"Food: {self.name}, {self.energy}"
