ea91ea44d4686993662a22dc900720e100f7329f
bloodtaint <bloodtaint@yahoo.com>
2022-07-21 12:57:19 +0300

    21.07.22

--- 19.exams/16_august_2020/project/software/light_software.py
+++ 19.exams/16_august_2020/project/software/light_software.py
@@ -4,6 +4,4 @@ from project.software.software import Software
 class LightSoftware(Software):
 
     def __init__(self, name: str, capacity_consumption: int, memory_consumption: int):
-        super().__init__(name, "Light", capacity_consumption=int(capacity_consumption * 1.5),
-                         memory_consumption=int(memory_consumption * 0.5))
-
+        super().__init__(name, "Light", int(capacity_consumption * 1.5), int(memory_consumption * 0.5))
