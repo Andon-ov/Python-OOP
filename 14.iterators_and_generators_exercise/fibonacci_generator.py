09335789d5efd983994b1e308e1647f293fb1a5f
BloodTaint <Bloodtaint@yahoo.com>
2022-07-23 16:20:19 +0300

    23.07.22

--- 14.iterators_and_generators_exercise/fibonacci_generator.py
+++ 14.iterators_and_generators_exercise/fibonacci_generator.py
@@ -1,5 +1,12 @@
 def fibonacci():
-    pass
+    first = 0
+    second = 1
+    yield first
+    yield second
+    while True:
+        result = first + second
+        first, second = second, result
+        yield result
 
 
 generator = fibonacci()
--- 14.iterators_and_generators_exercise/take_halves.py
+++ 14.iterators_and_generators_exercise/take_halves.py
@@ -9,8 +9,6 @@ def solution():
         for i in integers():
             yield i / 2
 
-    # TODO: Implement
-
     def take(n, seq):
         return [next(seq) for _ in range(n)]
 
