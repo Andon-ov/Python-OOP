c6a4cd29b4120512b298c24d8adc325c9672eb26
BloodTaint <Bloodtaint@yahoo.com>
2022-07-23 16:48:50 +0300

    23.07.22

--- 14.iterators_and_generators_exercise/possible_permutations.py
+++ 14.iterators_and_generators_exercise/possible_permutations.py
@@ -1,5 +1,9 @@
-def possible_permutations(param):
-    pass
+from itertools import permutations
+
+
+def possible_permutations(elements):
+    for result in permutations(elements):
+        yield list(result)
 
 
 [print(n) for n in possible_permutations([1, 2, 3])]
