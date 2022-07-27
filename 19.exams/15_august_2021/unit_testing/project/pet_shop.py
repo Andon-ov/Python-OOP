c591f366b466bbcfe017841423fc4dd85eccd631
BloodTaint <Bloodtaint@yahoo.com>
2022-07-27 17:28:44 +0300

    27.07.22

--- 19.exams/15_august_2021/unit_testing/project/pet_shop.py
+++ 19.exams/15_august_2021/unit_testing/project/pet_shop.py
@@ -36,3 +36,66 @@ class PetShop:
     def __repr__(self):
         return f'Shop {self.name}:\n' \
                f'Pets: {", ".join(self.pets)}'
+
+
+from unittest import TestCase, main
+
+
+class PetShopTest(TestCase):
+    def setUp(self) -> None:
+        self.name = 'My Pet Shop'
+        self.pet_shop = PetShop(self.name)
+        self.food = 'Food'
+        self.quantity = 100
+        self.invalid_quantity = 0
+        self.pet_name = 'My Pet'
+        self.pet_food = 'Pet Food'
+
+    # init
+    def test_pet_shop_init(self):
+        self.assertEqual(self.pet_shop.name, self.name)
+        self.assertEqual(self.pet_shop.pets, [])
+        self.assertEqual(self.pet_shop.food, {})
+
+    # add_food
+    def test_add_food_correctly(self):
+        result = self.pet_shop.add_food(self.food, self.quantity)
+        self.assertEqual(result, f"Successfully added {self.quantity:.2f} grams of {self.food}.")
+
+    def test_add_food_raise_value_error(self):
+        with self.assertRaises(ValueError) as ex:
+            self.pet_shop.add_food(self.food, self.invalid_quantity)
+        self.assertEqual(str(ex.exception), "Quantity cannot be equal to or less than 0")
+
+        # maybe i will make a test for negative num
+
+    # add_pet
+    def test_add_pet_work_currently(self):
+        result = self.pet_shop.add_pet(self.pet_name)
+        self.assertEqual(result, f"Successfully added {self.pet_name}.")
+
+        self.assertEqual(1, len(self.pet_shop.pets))
+
+    def test_add_pet_raise_exception(self):
+        self.pet_shop.add_pet(self.pet_name)
+        with self.assertRaises(Exception) as ex:
+            self.pet_shop.add_pet(self.pet_name)
+
+        self.assertEqual(str(ex.exception), "Cannot add a pet with the same name")
+
+    # feed_pet
+    def test_feed_pet_work_currently(self):
+        self.pet_shop.add_pet(self.pet_name)
+        self.pet_shop.add_food(self.pet_food,self.quantity)
+        result = self.pet_shop.feed_pet(self.pet_food, self.pet_name)
+
+        self.assertEqual(result, 'My Pet was successfully fed')
+        self.assertEqual(self.pet_shop.food, {'Pet Food': 0})
+
+    # def test_feed_pet
+    # def test_feed_pet
+    # def test_feed_pet
+    #def test__repr__
+
+if __name__ == '__main__':
+    main()
