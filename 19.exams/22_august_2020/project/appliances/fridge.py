aba0044aab6e648afa9635795f3ada6086cc48c7
BloodTaint <Bloodtaint@yahoo.com>
2022-07-31 17:34:54 +0300

    31.07.22

--- 19.exams/22_august_2020/project/appliances/fridge.py
+++ 19.exams/22_august_2020/project/appliances/fridge.py
@@ -3,5 +3,5 @@ from project.appliances.appliance import Appliance
 
 class Fridge(Appliance):
     cost = 1.2
-    def __init__(self, cost=1.2):
-        super().__init__(cost)
+    def __init__(self):
+        super().__init__(1.2)
--- 19.exams/22_august_2020/project/appliances/tv.py
+++ 19.exams/22_august_2020/project/appliances/tv.py
@@ -3,5 +3,6 @@ from project.appliances.appliance import Appliance
 
 class TV(Appliance):
     cost = 1.5
-    def __init__(self, cost=1.5):
-        super().__init__(cost)
+
+    def __init__(self):
+        super().__init__(1.5)
