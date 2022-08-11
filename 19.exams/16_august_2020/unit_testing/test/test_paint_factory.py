534e59f9c0d4afcf2e5013f991d48c75e7fcca08
BloodTaint <Bloodtaint@yahoo.com>
2022-08-11 10:24:11 +0300

    11.08.22

--- 19.exams/16_august_2020/unit_testing/test/test_paint_factory.py
+++ 19.exams/16_august_2020/unit_testing/test/test_paint_factory.py
@@ -1,135 +1,208 @@
 
-
 from unittest import TestCase, main
 
 from project.factory.paint_factory import PaintFactory
 
 
 class PaintFactoryTest(TestCase):
+    name: str = 'Test Factory'
+    capacity: int = 5
+
     def setUp(self) -> None:
-        pass
+        self.test_factory = PaintFactory(self.name, self.capacity)
 
-    def test__paint_factory_init__work_currently(self):
-        name: str = 'Test Factory'
-        capacity: int = 2
-        test_factory = PaintFactory(name, capacity)
+    def test_init_work_correctly(self):
+        self.assertEqual(self.test_factory.name, self.name)
+        self.assertEqual(self.test_factory.capacity, self.capacity)
+        self.assertListEqual(self.test_factory.valid_ingredients, ["white", "yellow", "blue", "green", "red"])
 
-        self.assertEqual(test_factory.name, name)
-        self.assertEqual(test_factory.capacity, capacity)
-        self.assertListEqual(test_factory.valid_ingredients, ["white", "yellow", "blue", "green", "red"])
-
-    def test__add_ingredient__work_currently(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
+    def test_add_ingredient_work_correctly(self):
         product_type = "white"
         product_quantity = 5
-        test_factory = PaintFactory(name, capacity)
-        test_factory.add_ingredient(product_type, product_quantity)
-        self.assertTrue(test_factory.ingredients[product_type])
-        self.assertEqual(test_factory.ingredients[product_type], 5)
+        self.test_factory.add_ingredient(product_type, product_quantity)
 
-    def test__add_ingredient__raise_value_error_not_enough_space(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
+        self.assertIn(product_type, self.test_factory.ingredients)
+        self.assertEqual(self.test_factory.ingredients[product_type], product_quantity)
+
+    def test_add_ingredient_raise_value_error_not_space_in_factory(self):
         product_type = "white"
-        product_quantity = 6
-        test_factory = PaintFactory(name, capacity)
+        product_quantity = 5
+        self.test_factory.add_ingredient(product_type, product_quantity)
+
+        second_product_type = "white"
+        second_product_quantity = 1
+
         with self.assertRaises(ValueError) as ex:
-            test_factory.add_ingredient(product_type, product_quantity)
+            self.test_factory.add_ingredient(second_product_type, second_product_quantity)
         self.assertEqual(str(ex.exception), "Not enough space in factory")
 
-    def test__add_ingredient__raise_type_error_ingredient_not_allowed(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
-        product_type = "white1"
+    def test_add_ingredient_raise_type_error_not_allowed_product(self):
+        product_type = "white_light"
         product_quantity = 5
-        test_factory = PaintFactory(name, capacity)
 
         with self.assertRaises(TypeError) as ex:
-            test_factory.add_ingredient(product_type, product_quantity)
-        self.assertEqual(str(ex.exception), 'Ingredient of type white1 not allowed in PaintFactory')
+            self.test_factory.add_ingredient(product_type, product_quantity)
+        self.assertEqual(str(ex.exception), f"Ingredient of type {product_type} not allowed in PaintFactory")
 
-    def test__remove_ingredient__work_currently(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
+    def test_remove_ingredient_work_correctly(self):
         product_type = "white"
-        product_quantity = 4
-        test_factory = PaintFactory(name, capacity)
-        test_factory.add_ingredient(product_type, product_quantity)
-        self.assertEqual(test_factory.ingredients[product_type], 4)
+        product_quantity = 5
+        self.test_factory.ingredients[product_type] = product_quantity
+        self.assertEqual(self.test_factory.ingredients, {product_type: product_quantity})
 
-        test_factory.remove_ingredient(product_type, 3)
-        self.assertEqual(test_factory.ingredients, {'white': 1})
+        self.test_factory.remove_ingredient(product_type, product_quantity)
+        self.assertEqual(self.test_factory.ingredients, {product_type: 0})
 
-    def test__remove_ingredient__raise_value_error__cannot_be_less_than_zero(self):
-        # "Ingredients quantity cannot be less than zero"
-        name: str = 'Test Factory'
-        capacity: int = 5
+    def test_remove_ingredient_raise_value_error_quantity_cannot_be_less_than_zero(self):
         product_type = "white"
-        product_quantity = 4
-        test_factory = PaintFactory(name, capacity)
-        test_factory.add_ingredient(product_type, product_quantity)
-        self.assertEqual(test_factory.ingredients[product_type], product_quantity)
+        product_quantity = 5
+        self.test_factory.ingredients[product_type] = product_quantity
+        self.assertEqual(self.test_factory.ingredients, {product_type: product_quantity})
 
         with self.assertRaises(ValueError) as ex:
-            test_factory.remove_ingredient(product_type, 10)
-        self.assertEqual(str(ex.exception), 'Ingredients quantity cannot be less than zero')
-        self.assertEqual(test_factory.ingredients, {product_type: product_quantity})
+            self.test_factory.remove_ingredient(product_type, 6)
+        self.assertEqual(str(ex.exception), "Ingredients quantity cannot be less than zero")
 
-    def test__remove_ingredient__raise_key_error__no_such_ingredient(self):
-        # KeyError("No such ingredient in the factory")
-        name: str = 'Test Factory'
-        capacity: int = 5
+    def test_remove_ingredient_raise_key_error_no_such_ingredient(self):
         product_type = "white"
-        product_quantity = 4
-        test_factory = PaintFactory(name, capacity)
-        test_factory.add_ingredient(product_type, product_quantity)
-        self.assertEqual(test_factory.ingredients[product_type], product_quantity)
-
+        product_quantity = 5
         with self.assertRaises(KeyError) as ex:
-            test_factory.remove_ingredient("white1", product_quantity)
+            self.test_factory.remove_ingredient(product_type, product_quantity)
         self.assertEqual(str(ex.exception), "'No such ingredient in the factory'")
-        self.assertEqual(test_factory.ingredients, {product_type: product_quantity})
-        # tuk moje da ima greshka
 
-    def test__can_add__work_currently(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
-        product_type = "white"
-        product_quantity = 4
-        test_factory = PaintFactory(name, capacity)
-        self.assertTrue(test_factory.can_add(product_quantity))
-
-    def test__can_add__False(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
-        product_type = "white"
-        product_quantity = 4
-        test_factory = PaintFactory(name, capacity)
-        self.assertFalse(test_factory.can_add(6))
-
-    def test__repr__work_currently(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
-        product_type = "white"
-        product_quantity = 5
-        test_factory = PaintFactory(name, capacity)
-        test_factory.add_ingredient(product_type, product_quantity)
-
-        actual_result = repr(test_factory)
-        expected_result = 'Factory name: Test Factory with capacity 5.\nwhite: 5\n'
-        self.assertEqual(actual_result, expected_result)
-
-    def test__property_products__work_currently(self):
-        name: str = 'Test Factory'
-        capacity: int = 5
-        product_type = "white"
-        product_quantity = 5
-        test_factory = PaintFactory(name, capacity)
-        test_factory.add_ingredient(product_type, product_quantity)
-
-        self.assertEqual(test_factory.products, {product_type: product_quantity})
+    def test_products_work_correctly(self):
+        self.assertEqual(self.test_factory.products, self.test_factory.ingredients)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
+
+# class PaintFactoryTest(TestCase):
+#     def setUp(self) -> None:
+#         pass
+#
+#     def test__paint_factory_init__work_currently(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 2
+#         test_factory = PaintFactory(name, capacity)
+#
+#         self.assertEqual(test_factory.name, name)
+#         self.assertEqual(test_factory.capacity, capacity)
+#         self.assertListEqual(test_factory.valid_ingredients, ["white", "yellow", "blue", "green", "red"])
+#
+#     def test__add_ingredient__work_currently(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 5
+#         test_factory = PaintFactory(name, capacity)
+#         test_factory.add_ingredient(product_type, product_quantity)
+#         self.assertTrue(test_factory.ingredients[product_type])
+#         self.assertEqual(test_factory.ingredients[product_type], 5)
+#
+#     def test__add_ingredient__raise_value_error_not_enough_space(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 6
+#         test_factory = PaintFactory(name, capacity)
+#         with self.assertRaises(ValueError) as ex:
+#             test_factory.add_ingredient(product_type, product_quantity)
+#         self.assertEqual(str(ex.exception), "Not enough space in factory")
+#
+#     def test__add_ingredient__raise_type_error_ingredient_not_allowed(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white1"
+#         product_quantity = 5
+#         test_factory = PaintFactory(name, capacity)
+#
+#         with self.assertRaises(TypeError) as ex:
+#             test_factory.add_ingredient(product_type, product_quantity)
+#         self.assertEqual(str(ex.exception), 'Ingredient of type white1 not allowed in PaintFactory')
+#
+#     def test__remove_ingredient__work_currently(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 4
+#         test_factory = PaintFactory(name, capacity)
+#         test_factory.add_ingredient(product_type, product_quantity)
+#         self.assertEqual(test_factory.ingredients[product_type], 4)
+#
+#         test_factory.remove_ingredient(product_type, 3)
+#         self.assertEqual(test_factory.ingredients, {'white': 1})
+#
+#     def test__remove_ingredient__raise_value_error__cannot_be_less_than_zero(self):
+#         # "Ingredients quantity cannot be less than zero"
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 4
+#         test_factory = PaintFactory(name, capacity)
+#         test_factory.add_ingredient(product_type, product_quantity)
+#         self.assertEqual(test_factory.ingredients[product_type], product_quantity)
+#
+#         with self.assertRaises(ValueError) as ex:
+#             test_factory.remove_ingredient(product_type, 10)
+#         self.assertEqual(str(ex.exception), 'Ingredients quantity cannot be less than zero')
+#         self.assertEqual(test_factory.ingredients, {product_type: product_quantity})
+#
+#     def test__remove_ingredient__raise_key_error__no_such_ingredient(self):
+#         # KeyError("No such ingredient in the factory")
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 4
+#         test_factory = PaintFactory(name, capacity)
+#         test_factory.add_ingredient(product_type, product_quantity)
+#         self.assertEqual(test_factory.ingredients[product_type], product_quantity)
+#
+#         with self.assertRaises(KeyError) as ex:
+#             test_factory.remove_ingredient("white1", product_quantity)
+#         self.assertEqual(str(ex.exception), "'No such ingredient in the factory'")
+#         self.assertEqual(test_factory.ingredients, {product_type: product_quantity})
+#         # tuk moje da ima greshka
+#
+#     def test__can_add__work_currently(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 4
+#         test_factory = PaintFactory(name, capacity)
+#         self.assertTrue(test_factory.can_add(product_quantity))
+#
+#     def test__can_add__False(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 4
+#         test_factory = PaintFactory(name, capacity)
+#         self.assertFalse(test_factory.can_add(6))
+#
+#     def test__repr__work_currently(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 5
+#         test_factory = PaintFactory(name, capacity)
+#         test_factory.add_ingredient(product_type, product_quantity)
+#
+#         actual_result = repr(test_factory)
+#         expected_result = 'Factory name: Test Factory with capacity 5.\nwhite: 5\n'
+#         self.assertEqual(actual_result, expected_result)
+#
+#     def test__property_products__work_currently(self):
+#         name: str = 'Test Factory'
+#         capacity: int = 5
+#         product_type = "white"
+#         product_quantity = 5
+#         test_factory = PaintFactory(name, capacity)
+#         test_factory.add_ingredient(product_type, product_quantity)
+#
+#         self.assertEqual(test_factory.products, {product_type: product_quantity})
+#
+#
+# if __name__ == "__main__":
+#     main()
--- 19.exams/22_august_2020/unit_testing/project/test/test.py
+++ 19.exams/22_august_2020/unit_testing/project/test/test.py
@@ -1,117 +1,189 @@
+
 from project.student_report_card import StudentReportCard
 
 from unittest import TestCase, main
 
 
 class StudentReportCardTest(TestCase):
+    student_name: str = "Kolio"
+    school_year: int = 10
+
     def setUp(self) -> None:
-        self.student_name: str = 'Kolio'
-        self.school_year: int = 11
+        self.test = StudentReportCard(self.student_name, self.school_year)
 
-        self.test_student = StudentReportCard(self.student_name, self.school_year)
+    def test_init_work_correctly(self):
+        self.assertEqual(self.test.student_name, self.student_name)
+        self.assertEqual(self.test.school_year, self.school_year)
+        self.assertEqual(self.test.grades_by_subject, {})
 
-    def test_student_report_card_init(self):
-        self.assertEqual(self.test_student.student_name, self.student_name)
-        self.assertEqual(self.test_student.school_year, self.school_year)
-        self.assertEqual(self.test_student.grades_by_subject, {})
-
-    # def test_student_name_setter_work_currently(self):
-    #     new_student_name = 'Ivan'
-    #
-    #     self.test_student.student_name = new_student_name
-    #     result = self.test_student.student_name
-    #     self.assertEqual(result, new_student_name)
-
-    def test_student_name__raise_value_error_student__name_cannot_be_empty_string(self):
-        new_student_name = ''
+    def test_student_name_raise_value_error(self):
         with self.assertRaises(ValueError) as ex:
-            self.test_student.student_name = new_student_name
+            self.test.student_name = ''
         self.assertEqual(str(ex.exception), "Student Name cannot be an empty string!")
 
-    # def test__school_year_setter__work_currently(self):
-    #     actual_result = self.test_student.school_year = 10
-    #     expected_result = self.test_student.school_year
-    #     self.assertEqual(actual_result, expected_result)
-
-    def test_student_name__raise_value_error__year_must_be_between_1_and_12(self):
-        new_school_year = 13
+    def test_school_year_raise_value_error(self):
         with self.assertRaises(ValueError) as ex:
-            self.test_student.school_year = new_school_year
+            self.test.school_year = 13
         self.assertEqual(str(ex.exception), "School Year must be between 1 and 12!")
 
-    # def test_add_grade_work_currently(self):
-    #     grade = 4
-    #     self.test_student.add_grade(self.student_name, grade)
-    #     other_test_student = 'Joro'
-    #     self.test_student.add_grade(other_test_student, grade)
-    #
-    #     self.assertIn(self.student_name, self.test_student.grades_by_subject)
-    #     self.assertIn(grade, self.test_student.grades_by_subject[self.student_name])
-    #
-    #     actual_result = self.test_student.grades_by_subject
-    #     expected_result = {'Joro': [4], 'Kolio': [4]}
-    #     self.assertEqual(actual_result, expected_result)
+    def test_work_correctly_test_the_low_limits(self):
+        first_year = 1
+        self.test.school_year = first_year
+        self.assertEqual(first_year, self.test.school_year)
 
-    # def test_add_grade_if_obj_already_in(self):
-    #     grade = 6
-    #     self.test_student.add_grade(self.student_name, grade)
-    #
-    #     self.test_student.add_grade(self.student_name, grade)
-    #     actual_result = self.test_student.grades_by_subject
-    #     expected_result = {self.student_name: [grade, grade]}
-    #     self.assertEqual(actual_result, expected_result)
+    def test_work_correctly_test_the_high_limits(self):
+        last_year = 12
+        self.test.school_year = last_year
+        self.assertEqual(last_year, self.test.school_year)
 
     def test_average_grade_by_subject(self):
         grade = 6
         other_grade = 4
         other_test_student = 'Joro'
         for _ in range(3):
-            self.test_student.add_grade(self.student_name, grade)
+            self.test.add_grade(self.student_name, grade)
+            self.test.add_grade(other_test_student, other_grade)
 
-            self.test_student.add_grade(other_test_student, other_grade)
-
-        actual_result = self.test_student.grades_by_subject
+        actual_result = self.test.grades_by_subject
         expected_result = {'Joro': [4, 4, 4], 'Kolio': [6, 6, 6]}
         self.assertEqual(actual_result, expected_result)
 
-        # self.assertEqual(len(self.test_student.grades_by_subject[self.test_student.student_name]), 3)
-        # self.assertEqual(len(self.test_student.grades_by_subject[other_test_student]), 3)
+        self.assertEqual(self.test.average_grade_by_subject(), 'Kolio: 6.00\nJoro: 4.00')
 
-        self.assertEqual(self.test_student.average_grade_by_subject(), 'Kolio: 6.00\nJoro: 4.00')
-
-    def test_average_grade_for_all_subjects(self):
+    def test_average_grade_for_all_subjects_work_correctly(self):
+        name = 'Pesho'
         grade = 6
-        other_grade = 4
-        other_test_student = 'Joro'
+        self.test.add_grade(self.student_name, grade)
+        self.test.add_grade(name, grade)
+        self.test.grades_by_subject[self.student_name].extend([6, 3, 4, 5])
+        self.test.grades_by_subject[name].extend([2, 3, 4, 3])
 
-        self.test_student.add_grade(self.student_name, grade)
-        self.test_student.add_grade(self.student_name, grade)
-        self.test_student.add_grade(self.student_name, grade)
-        self.test_student.add_grade(other_test_student, other_grade)
-        self.test_student.add_grade(other_test_student, other_grade)
-        self.test_student.add_grade(other_test_student, grade)
+        result = self.test.average_grade_for_all_subjects()
+        self.assertEqual(result, 'Average Grade: 4.20')
 
-        actual_result = self.test_student.average_grade_for_all_subjects()
-        expected_result = 'Average Grade: 5.33'
-
-        self.assertEqual(actual_result, expected_result)
-
-    def test_repr_work_currently(self):
+    def test___repr___work_correctly(self):
         grade = 6
-        other_grade = 4
-        other_test_student = 'Joro'
+        self.test.add_grade(self.student_name, grade)
 
-        self.test_student.add_grade(self.student_name, grade)
-        self.test_student.add_grade(self.student_name, grade)
-        self.test_student.add_grade(self.student_name, grade)
-        self.test_student.add_grade(other_test_student, other_grade)
-        self.test_student.add_grade(other_test_student, other_grade)
-        self.test_student.add_grade(other_test_student, grade)
+        self.test.grades_by_subject[self.student_name].extend([6, 3, 4, 5])
 
-        actual_result = repr(self.test_student)
-        result = f"Name: {self.student_name}\nYear: {self.school_year}\n----------\n{self.test_student.average_grade_by_subject()}\n----------\n{self.test_student.average_grade_for_all_subjects()}"
-        self.assertEqual(actual_result, result)
+        result = repr(self.test)
+        self.assertEqual(result, 'Name: Kolio\nYear: 10\n----------\nKolio: 4.80\n----------\nAverage Grade: 4.80')
 
 
 if __name__ == '__main__':
     main()
+
+#
+# class StudentReportCardTest(TestCase):
+#     def setUp(self) -> None:
+#         self.student_name: str = 'Kolio'
+#         self.school_year: int = 11
+#
+#         self.test_student = StudentReportCard(self.student_name, self.school_year)
+#
+#     def test_student_report_card_init(self):
+#         self.assertEqual(self.test_student.student_name, self.student_name)
+#         self.assertEqual(self.test_student.school_year, self.school_year)
+#         self.assertEqual(self.test_student.grades_by_subject, {})
+#
+#     # def test_student_name_setter_work_currently(self):
+#     #     new_student_name = 'Ivan'
+#     #
+#     #     self.test_student.student_name = new_student_name
+#     #     result = self.test_student.student_name
+#     #     self.assertEqual(result, new_student_name)
+#
+#     def test_student_name__raise_value_error_student__name_cannot_be_empty_string(self):
+#         new_student_name = ''
+#         with self.assertRaises(ValueError) as ex:
+#             self.test_student.student_name = new_student_name
+#         self.assertEqual(str(ex.exception), "Student Name cannot be an empty string!")
+#
+#     # def test__school_year_setter__work_currently(self):
+#     #     actual_result = self.test_student.school_year = 10
+#     #     expected_result = self.test_student.school_year
+#     #     self.assertEqual(actual_result, expected_result)
+#
+#     def test_student_name__raise_value_error__year_must_be_between_1_and_12(self):
+#         new_school_year = 13
+#         with self.assertRaises(ValueError) as ex:
+#             self.test_student.school_year = new_school_year
+#         self.assertEqual(str(ex.exception), "School Year must be between 1 and 12!")
+#
+#     # def test_add_grade_work_currently(self):
+#     #     grade = 4
+#     #     self.test_student.add_grade(self.student_name, grade)
+#     #     other_test_student = 'Joro'
+#     #     self.test_student.add_grade(other_test_student, grade)
+#     #
+#     #     self.assertIn(self.student_name, self.test_student.grades_by_subject)
+#     #     self.assertIn(grade, self.test_student.grades_by_subject[self.student_name])
+#     #
+#     #     actual_result = self.test_student.grades_by_subject
+#     #     expected_result = {'Joro': [4], 'Kolio': [4]}
+#     #     self.assertEqual(actual_result, expected_result)
+#
+#     # def test_add_grade_if_obj_already_in(self):
+#     #     grade = 6
+#     #     self.test_student.add_grade(self.student_name, grade)
+#     #
+#     #     self.test_student.add_grade(self.student_name, grade)
+#     #     actual_result = self.test_student.grades_by_subject
+#     #     expected_result = {self.student_name: [grade, grade]}
+#     #     self.assertEqual(actual_result, expected_result)
+#
+#     def test_average_grade_by_subject(self):
+#         grade = 6
+#         other_grade = 4
+#         other_test_student = 'Joro'
+#         for _ in range(3):
+#             self.test_student.add_grade(self.student_name, grade)
+#
+#             self.test_student.add_grade(other_test_student, other_grade)
+#
+#         actual_result = self.test_student.grades_by_subject
+#         expected_result = {'Joro': [4, 4, 4], 'Kolio': [6, 6, 6]}
+#         self.assertEqual(actual_result, expected_result)
+#
+#         # self.assertEqual(len(self.test_student.grades_by_subject[self.test_student.student_name]), 3)
+#         # self.assertEqual(len(self.test_student.grades_by_subject[other_test_student]), 3)
+#
+#         self.assertEqual(self.test_student.average_grade_by_subject(), 'Kolio: 6.00\nJoro: 4.00')
+#
+#     def test_average_grade_for_all_subjects(self):
+#         grade = 6
+#         other_grade = 4
+#         other_test_student = 'Joro'
+#
+#         self.test_student.add_grade(self.student_name, grade)
+#         self.test_student.add_grade(self.student_name, grade)
+#         self.test_student.add_grade(self.student_name, grade)
+#         self.test_student.add_grade(other_test_student, other_grade)
+#         self.test_student.add_grade(other_test_student, other_grade)
+#         self.test_student.add_grade(other_test_student, grade)
+#
+#         actual_result = self.test_student.average_grade_for_all_subjects()
+#         expected_result = 'Average Grade: 5.33'
+#
+#         self.assertEqual(actual_result, expected_result)
+#
+#     def test_repr_work_currently(self):
+#         grade = 6
+#         other_grade = 4
+#         other_test_student = 'Joro'
+#
+#         self.test_student.add_grade(self.student_name, grade)
+#         self.test_student.add_grade(self.student_name, grade)
+#         self.test_student.add_grade(self.student_name, grade)
+#         self.test_student.add_grade(other_test_student, other_grade)
+#         self.test_student.add_grade(other_test_student, other_grade)
+#         self.test_student.add_grade(other_test_student, grade)
+#
+#         actual_result = repr(self.test_student)
+#         result = f"Name: {self.student_name}\nYear: {self.school_year}\n----------\n{self.test_student.average_grade_by_subject()}\n----------\n{self.test_student.average_grade_for_all_subjects()}"
+#         self.assertEqual(actual_result, result)
+#
+#
+# if __name__ == '__main__':
+#     main()
